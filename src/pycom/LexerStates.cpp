67af3231b69241b22d0f663e0fe1763e9740620f
Ploshka <folga12381@gmail.com>
2023-04-07 23:55:44 +1000

    Полностью переработал состояния

--- src/pycom/LexerStates.cpp
+++ src/pycom/LexerStates.cpp
@@ -1,271 +1,237 @@
 #include <pycom/lexer/LexerStates.hpp>
+#include <unordered_map>
+#include <functional>
 
-#define impl(name) name::name(LexerInterface* lex, std::string& acc, Type& t, unsigned int& row, unsigned int& pos):BaseLexerState(lex, acc, t, row, pos){} \
-                   bool name::recognize(char c)
+#define impl(name)                                                                                                                           \
+    name::name(LexerInterface *lex, std::string &acc, Type &t, unsigned int &row, unsigned int &pos) : BaseLexerState(lex, acc, t, row, pos) \
+    {                                                                                                                                        \
+    }                                                                                                                                        \
+    bool name::recognize(char c)
 #define newstate(name) lexer->setState(new name(lexer, accum, type, row, pos))
 #define toacc accum.push_back(c)
-#define argtoacc(arg) accum.push_back(arg);
-#define singlesymbol(sym, state, t) case sym: \
-                                    newstate(state); \
-                                    type = t; \
-                                    return true; \
-                                    break
-#define justswitch(symbol, state) case symbol: \
-                                    newstate(state); \
-                                    break
+#define tablestate lexer->setState(table[c](lexer, accum, type, row, pos))
+#define fac(state)                                                                                                           \
+    inline LexerStateInterface *state##Factory(LexerInterface *a, std::string &b, Type &c, unsigned int &d, unsigned int &e) \
+    {                                                                                                                        \
+        return new state(a, b, c, d, e);                                                                                     \
+    }
 
-inline bool isSuitableForIdBeginning(char c) {
+fac(String)
+fac(Colon)
+fac(Dot)
+fac(Plus)
+fac(Minus)
+fac(Star)
+fac(Div)
+fac(Mod)
+fac(Matmul)
+fac(Greater)
+fac(Less)
+fac(Assign)
+fac(Inv)
+fac(Band)
+fac(Bor)
+fac(Xor)
+fac(Lpr)
+fac(Rpr)
+fac(Lsbr)
+fac(Rsbr)
+fac(Lbr)
+fac(Rbr)
+fac(Exclamation)
+fac(Newline)
+fac(Comment)
+fac(Comma)
+
+#define tab(symbol, state)     \
+    {                          \
+        symbol, state##Factory \
+    }
+
+static std::unordered_map<char, std::function<LexerStateInterface *(LexerInterface *,
+                                                                    std::string &,
+                                                                    Type &,
+                                                                    unsigned int &,
+                                                                    unsigned int &)>> table = {
+    tab('+', Plus),
+    tab('-', Minus),
+    tab('*', Star),
+    tab('/', Div),
+    tab('@', Matmul),
+    tab('%', Mod),
+    tab('&', Band),
+    tab('|', Bor),
+    tab('^', Xor),
+    tab('!', Exclamation),
+    tab('<', Less),
+    tab('>', Greater),
+    tab('=', Assign),
+    tab('~', Inv),
+    tab('.', Dot),
+    tab(',', Comma),
+    tab('(', Lpr),
+    tab(')', Rpr),
+    tab('[', Lsbr),
+    tab(']', Rsbr),
+    tab('{', Lbr),
+    tab('}', Rbr),
+    tab('#', Comment),
+    tab(':', Colon),
+    tab('"', String),
+    tab('\'', String),
+    tab('\n', Newline),
+};
+
+inline bool isSuitableForIdBeginning(char c)
+{
     return std::isalpha(c) || c == '_';
 }
 
-inline bool isSuitableForId(char c) {
+inline bool isSuitableForId(char c)
+{
     return std::isalnum(c) || c == '_';
 }
 
-BaseLexerState::BaseLexerState(LexerInterface* lex, std::string& acc, Type& t, unsigned int& row, unsigned int& pos):
-    lexer(lex), accum(acc), type(t), row(row), pos(pos) {}
+BaseLexerState::BaseLexerState(LexerInterface *lex, std::string &acc, Type &t, unsigned int &row, unsigned int &pos) : lexer(lex), accum(acc), type(t), row(row), pos(pos) {}
 
-impl(Start) {
+impl(Start)
+{
     pos++;
-    if (isSuitableForIdBeginning(c)) {
+    if (isSuitableForIdBeginning(c))
+    {
         toacc;
         newstate(Id);
-    } else if (std::isdigit(c)) {
+    }
+    else if (std::isdigit(c))
+    {
         toacc;
         newstate(FirstNumPart);
-    } else {
-        switch (c) {
-        case ' ':
-            break;
-
-        case '\n':
-            row++;
-            pos = 0;
-            break;
-
-        justswitch('#', Comment);
-        justswitch('(', Lpr);
-        justswitch('{', Lbr);
-        justswitch('[', Lsbr);
-        justswitch('+', Plus);
-        justswitch('-', Minus);
-        justswitch('~', Inv);
-        justswitch('"', String);
-        justswitch('\'', String);
-
-        default:
-            type = Type::unexpected;
-            return true;
-            break;
-        }
+    }
+    else
+    {
+        tablestate;
     }
     return false;
 }
 
-impl(Space) {
+impl(Skip)
+{
     pos++;
-    if (isSuitableForIdBeginning(c)) {
+    if (isSuitableForIdBeginning(c))
+    {
         toacc;
         newstate(Id);
-    } else if (std::isdigit(c)) {
+    }
+    else if (std::isdigit(c))
+    {
         toacc;
         newstate(FirstNumPart);
-    } else {
-        switch (c) {
-        case ' ':
-            break;
-
-        justswitch('+', Plus);
-        justswitch('-', Minus);
-        justswitch('*', Star);
-        justswitch('/', Div);
-        justswitch('@', Matmul);
-        justswitch('<', Less);
-        justswitch('>', Greater);
-        justswitch('=', Assign);
-        justswitch('%', Mod);
-        justswitch('&', Band);
-        justswitch('|', Bor);
-        justswitch('^', Xor);
-        justswitch('(', Lpr);
-        justswitch(')', Rpr);
-        justswitch('[', Lsbr);
-        justswitch(']', Rsbr);
-        justswitch('{', Lbr);
-        justswitch('}', Rbr);
-        justswitch(',', Comma);
-        justswitch('.', Dot);
-        justswitch('\n', Newline);
-        }
+    }
+    else
+    {
+        tablestate;
     }
     return false;
 }
 
-impl(Id) {
+impl(Id)
+{
     pos++;
-    if (isSuitableForId(c)) {
+    if (isSuitableForId(c))
+    {
         toacc;
-    } else {
+    }
+    else
+    {
+        tablestate;
         type = Type::id;
-        switch (c)
-        {
-        justswitch(' ', Space);
-        justswitch('+', Plus);
-        justswitch('-', Minus);
-        justswitch('*', Star);
-        justswitch('/', Div);
-        justswitch('@', Matmul);
-        justswitch('<', Less);
-        justswitch('>', Greater);
-        justswitch('=', Assign);
-        justswitch('%', Mod);
-        justswitch('&', Band);
-        justswitch('|', Bor);
-        justswitch('^', Xor);
-        justswitch('(', Lpr);
-        justswitch(')', Rpr);
-        justswitch('[', Lsbr);
-        justswitch(']', Rsbr);
-        justswitch('{', Lbr);
-        justswitch('}', Rbr);
-        justswitch(',', Comma);
-        justswitch('.', Dot);
-        justswitch('\n', Newline);
-
-        default:
-            type = Type::unexpected;
-            break;
-        }
         return true;
     }
     return false;
 }
 
-impl(String) {
+impl(String)
+{
     pos++;
-    if (c == '\'' || c == '"') {
+    if (c == '\'' || c == '"')
+    {
         type = Type::string;
-        newstate(Space);
+        newstate(Skip);
         return true;
-    } else {
+    }
+    else
+    {
         toacc;
         return false;
     }
 }
 
-impl(Colon) {
+impl(Colon)
+{
     pos++;
-    if (isSuitableForIdBeginning(c)) {
+    if (isSuitableForIdBeginning(c))
+    {
         newstate(Id);
-    } else if (std::isdigit(c)) {
+    }
+    else if (std::isdigit(c))
+    {
         newstate(FirstNumPart);
-    } else {
-        switch (c) {
-        justswitch('+', Plus);
-        justswitch('-', Minus);
-        justswitch('~', Inv);
-        justswitch('"', String);
-        justswitch('\'', String);
-        justswitch('\n', Newline);
-        justswitch(' ', Space);            
-        justswitch('#', Comment);
-
-        default:
-            type = Type::unexpected;
-            return true;
-            break;
-        }
+    }
+    else
+    {
+        tablestate;
     }
     type = Type::colon;
     return true;
 }
 
-impl(Dot) {
+impl(Dot)
+{
     pos++;
-    if (isSuitableForIdBeginning(c)) {
-        type = Type::dot;
+    if (isSuitableForIdBeginning(c))
+    {
         newstate(Id);
-        return true;
-    } else {
-        type = Type::unexpected;
-        return true;
     }
+    else
+    {
+        tablestate;
+    }
+    type = Type::dot;
+    return true;
 }
 
-impl(FirstNumPart) {
+impl(FirstNumPart)
+{
     pos++;
-    if (std::isdigit(c)) {
+    if (std::isdigit(c))
+    {
         toacc;
-    } else if (c == '.') {
+    }
+    else if (c == '.')
+    {
         toacc;
         newstate(SecondNumPart);
-    } else {
+    }
+    else
+    {
+        tablestate;
         type = Type::number;
-        switch (c) {
-        justswitch(' ', Space);
-        justswitch('+', Plus);
-        justswitch('-', Minus);
-        justswitch('*', Star);
-        justswitch('/', Div);
-        justswitch('@', Matmul);
-        justswitch('<', Less);
-        justswitch('>', Greater);
-        justswitch('=', Assign);
-        justswitch('%', Mod);
-        justswitch('&', Band);
-        justswitch('|', Bor);
-        justswitch('^', Xor);
-        justswitch('(', Lpr);
-        justswitch(')', Rpr);
-        justswitch('[', Lsbr);
-        justswitch(']', Rsbr);
-        justswitch('{', Lbr);
-        justswitch('}', Rbr);
-        justswitch(',', Comma);
-        justswitch('\n', Newline);
-
-        default:
-            type = Type::unexpected;
-            break;
-        }
         return true;
     }
     return false;
 }
 
-impl(SecondNumPart) {
+impl(SecondNumPart)
+{
     pos++;
-    if (std::isdigit(c)) {
+    if (std::isdigit(c))
+    {
         toacc;
-    } else {
+    }
+    else
+    {
+        tablestate;
         type = Type::number;
-        switch (c) {
-        justswitch(' ', Space);
-        justswitch('+', Plus);
-        justswitch('-', Minus);
-        justswitch('*', Star);
-        justswitch('/', Div);
-        justswitch('@', Matmul);
-        justswitch('<', Less);
-        justswitch('>', Greater);
-        justswitch('=', Assign);
-        justswitch('%', Mod);
-        justswitch('&', Band);
-        justswitch('|', Bor);
-        justswitch('^', Xor);
-        justswitch('(', Lpr);
-        justswitch(')', Rpr);
-        justswitch('[', Lsbr);
-        justswitch(']', Rsbr);
-        justswitch('{', Lbr);
-        justswitch('}', Rbr);
-        justswitch(',', Comma);
-        justswitch('\n', Newline);
-
-        default:
-            type = Type::unexpected;
-            break;
-        }
         return true;
     }
     return false;
@@ -291,24 +257,9 @@ impl(Rsbr) {}
 impl(Lbr) {}
 impl(Rbr) {}
 impl(Idiv) {}
-impl(Equal) {}
-impl(Noteq) {}
-impl(Grequal) {}
-impl(Lequal) {}
 impl(Lshift) {}
 impl(Rshift) {}
-impl(Plusass) {}
-impl(Minass) {}
-impl(Mulass) {}
-impl(Divass) {}
-impl(Modass) {}
-impl(Idivass) {}
-impl(Matmulass) {}
-impl(Lshiftass) {}
-impl(Rshiftass) {}
-impl(Bandass) {}
-impl(Borass) {}
-impl(Xorass) {}
+impl(Exclamation) {}
 impl(Newline) {}
 impl(Comment) {}
 impl(Comma) {}
