d6b598411c7bb789a00a76e85c849667aac2c360
Limuran <strelgl2@gmail.com>
2023-06-17 22:06:55 +1000

    Визитёр для принта узлов синтаксического дерева

--- include/pycom/interface/NodeVisitorInterface.hpp
+++ include/pycom/interface/NodeVisitorInterface.hpp
@@ -6,21 +6,21 @@ class NodeVisitorInterface
 public:
     virtual ~NodeVisitorInterface() = default;
 
-    virtual void visitLeaf(Leaf* acceptor) const = 0;
-    virtual void visitFormalParamsNode(FormalParamsNode* acceptor) const = 0;
-    virtual void visitActualParamsNode(ActualParamsNode* acceptor) const = 0;
-    virtual void visitVariableNode(VariableNode* acceptor) const = 0;
-    virtual void visitCallNode(CallNode* acceptor) const = 0;
-    virtual void visitBinaryNode(BinaryNode* acceptor) const = 0;
-    virtual void visitUnaryNode(UnaryNode* acceptor) const = 0;
-    virtual void visitAssignmentNode(AssignmentNode* acceptor) const = 0;
-    virtual void visitReturnNode(ReturnNode* acceptor) const = 0;
-    virtual void visitBlockNode(BlockNode* acceptor) const = 0;
-    virtual void visitProgramNode(ProgramNode* acceptor) const = 0;
-    virtual void visitFunctionNode(FunctionNode* acceptor) const = 0;
-    virtual void visitElseNode(ElseNode* acceptor) const = 0;
-    virtual void visitElifNode(ElifNode* acceptor) const = 0;
-    virtual void visitIfNode(IfNode* acceptor) const = 0;
-    virtual void visitWhileNode(WhileNode* acceptor) const = 0;
-    virtual void visitForNode(ForNode* acceptor) const = 0;
+    virtual void visitLeaf(Leaf* acceptor) = 0;
+    virtual void visitFormalParamsNode(FormalParamsNode* acceptor) = 0;
+    virtual void visitActualParamsNode(ActualParamsNode* acceptor) = 0;
+    virtual void visitVariableNode(VariableNode* acceptor) = 0;
+    virtual void visitCallNode(CallNode* acceptor) = 0;
+    virtual void visitBinaryNode(BinaryNode* acceptor) = 0;
+    virtual void visitUnaryNode(UnaryNode* acceptor) = 0;
+    virtual void visitAssignmentNode(AssignmentNode* acceptor) = 0;
+    virtual void visitReturnNode(ReturnNode* acceptor) = 0;
+    virtual void visitBlockNode(BlockNode* acceptor) = 0;
+    virtual void visitProgramNode(ProgramNode* acceptor) = 0;
+    virtual void visitFunctionNode(FunctionNode* acceptor) = 0;
+    virtual void visitElseNode(ElseNode* acceptor) = 0;
+    virtual void visitElifNode(ElifNode* acceptor) = 0;
+    virtual void visitIfNode(IfNode* acceptor) = 0;
+    virtual void visitWhileNode(WhileNode* acceptor) = 0;
+    virtual void visitForNode(ForNode* acceptor) = 0;
 };
