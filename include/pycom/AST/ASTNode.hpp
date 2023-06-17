5d0042c2b243fa839991825241b30fa07c304323
Limuran <strelgl2@gmail.com>
2023-06-17 17:09:58 +1000

    Узлы синтаксического дерева

--- include/pycom/AST/ASTNode.hpp
+++ include/pycom/AST/ASTNode.hpp
@@ -1,7 +1,146 @@
 #pragma once
+
 #include <pycom/interface/ASTNodeInterface.hpp>
 #include <pycom/interface/VisitableNodeInterface.hpp>
+#include <pycom/token/Token.hpp>
+#include <vector>
 
-class BaseASTNode : public ASTNodeInterface, public VisitableNodeInterface
-{
+class BaseASTNode : public ASTNodeInterface, public VisitableNodeInterface {
+public:
+    void accept(NodeVisitorInterface *_visitor) override {};
 };
+
+
+// ============= Expressions =============
+class ExpressionNode : public BaseASTNode {
+};  // Абстрактный класс выражения
+
+class Leaf : public ExpressionNode {
+public:
+    Token token;
+    Leaf(Token token);
+};
+
+
+// ============= Params =============
+class FormalParamsNode : public BaseASTNode {
+public:
+    std::vector<Leaf*> params;
+    FormalParamsNode(std::vector<Leaf*> params);
+    void add_child(Leaf* param);
+};
+
+class ActualParamsNode : public BaseASTNode {
+public:
+    std::vector<ExpressionNode*> params;
+    ActualParamsNode(std::vector<ExpressionNode*> params);
+    void add_child(ExpressionNode* param);
+};
+
+
+// ============= Expressions =============
+class VariableNode: public ExpressionNode {
+public:
+    std::vector<Leaf*> chain;  // Цепочка обращений. Например: ID.ID.ID, либо ID, либо NUMBER
+    VariableNode(std::vector<Leaf*> chain);
+    void add_to_chain(Leaf * leaf);
+};
+
+class CallNode : public ExpressionNode {
+public:
+    VariableNode* callable;
+    ActualParamsNode* params;
+    CallNode(VariableNode* callable, ActualParamsNode* params);
+};
+
+class BinaryNode : public ExpressionNode {
+public:
+    ExpressionNode* left;
+    Leaf* op;
+    ExpressionNode* right;
+    BinaryNode(ExpressionNode* left, Leaf *op, ExpressionNode* right);
+};
+
+class UnaryNode : public ExpressionNode {
+public:
+    Leaf* op;
+    ExpressionNode* operand;
+    UnaryNode(Leaf *op, ExpressionNode* operand);
+};
+
+class AssignmentNode : public BaseASTNode {
+public:
+    Leaf* left;
+    ExpressionNode* right;
+    AssignmentNode(Leaf* left, ExpressionNode* right);
+};
+
+class ReturnNode : public BaseASTNode {
+public:
+    ExpressionNode* return_value;
+    ReturnNode(ExpressionNode* return_value);
+};
+
+
+// ============= Block =============
+
+class BlockNode : public BaseASTNode {
+public:
+    std::vector<BaseASTNode*> children;
+    BlockNode(std::vector<BaseASTNode*> children);
+    BlockNode();
+
+    void add_child(BaseASTNode* child);
+};
+class ProgramNode : public BlockNode {};
+
+// ============= Compound statements =============
+
+class FunctionNode : public BaseASTNode {
+public:
+    Leaf* id;
+    FormalParamsNode* formal_params;
+    BlockNode* body;
+    FunctionNode(Leaf* id, FormalParamsNode* formal_params, BlockNode* body);
+};
+
+class ElseNode : public BaseASTNode {
+public:
+    BlockNode* body;
+    ElseNode(BlockNode *body);
+};
+
+class ElifNode : public BaseASTNode {
+public:
+    ExpressionNode* condition;
+    BlockNode* body;
+    ElifNode* next_elif;
+    ElseNode* next_else;
+    ElifNode(ExpressionNode* condition, BlockNode* body);
+};
+
+class IfNode : public BaseASTNode {
+public:
+    ExpressionNode* condition;
+    BlockNode* body;
+    ElifNode* next_elif;
+    ElseNode* next_else;
+    IfNode(ExpressionNode* condition, BlockNode* body);
+};
+
+
+class WhileNode : public BaseASTNode {
+public:
+    ExpressionNode* condition;
+    BlockNode* body;
+    WhileNode(ExpressionNode* condition, BlockNode* body);
+};
+
+class ForNode : public BaseASTNode {
+public:
+    Leaf* iterator;
+    ExpressionNode* condition;
+    BlockNode* body;
+    ForNode(Leaf* iterator, ExpressionNode* condition, BlockNode* body);
+};
+
--- src/pycom/ASTNode.cpp
+++ src/pycom/ASTNode.cpp
@@ -1,1 +1,59 @@
 #include <pycom/AST/ASTNode.hpp>
+
+Leaf::Leaf(Token token) : token(token) {}
+
+VariableNode::VariableNode(std::vector<Leaf*> chain) : chain(chain) {}
+
+void VariableNode::add_to_chain(Leaf *leaf) {
+    this->chain.push_back(leaf);
+}
+
+ActualParamsNode::ActualParamsNode(std::vector<ExpressionNode*> params) : params(params) {}
+
+void ActualParamsNode::add_child(ExpressionNode *param) {
+    this->params.push_back(param);
+}
+
+CallNode::CallNode(VariableNode* callable, ActualParamsNode* params) : callable(callable), params(params) {}
+
+BinaryNode::BinaryNode(ExpressionNode* left, Leaf *op, ExpressionNode* right) : left(left), op(op), right(right) {}
+
+UnaryNode::UnaryNode(Leaf *op, ExpressionNode* operand) : op(op), operand(operand) {}
+
+AssignmentNode::AssignmentNode(Leaf* left, ExpressionNode* right) : left(left), right(right) {}
+
+ReturnNode::ReturnNode(ExpressionNode* return_value) : return_value(return_value) {}
+
+BlockNode::BlockNode(std::vector<BaseASTNode*> children) : children(children) {}
+BlockNode::BlockNode(): children({}) {}
+
+FunctionNode::FunctionNode(Leaf* id, FormalParamsNode* formal_params, BlockNode* body) : id(id),
+                                                                                       formal_params(formal_params),
+                                                                                       body(body) {}
+
+IfNode::IfNode(ExpressionNode* condition, BlockNode* body) : condition(condition), body(body) {
+    this->next_elif = nullptr;
+    this->next_else = nullptr;
+}
+
+ElifNode::ElifNode(ExpressionNode* condition, BlockNode* body) : condition(condition), body(body) {
+    this->next_elif = nullptr;
+    this->next_else = nullptr;
+}
+
+ElseNode::ElseNode(BlockNode *body) : body(body) {}
+
+WhileNode::WhileNode(ExpressionNode* condition, BlockNode* body) : condition(condition), body(body) {}
+
+ForNode::ForNode(Leaf* iterator, ExpressionNode* condition, BlockNode* body) : iterator(iterator), condition(condition),
+                                                                             body(body) {}
+
+void BlockNode::add_child(BaseASTNode* child) {
+    this->children.push_back(child);
+}
+
+FormalParamsNode::FormalParamsNode(std::vector<Leaf *> params): params(params) {}
+
+void FormalParamsNode::add_child(Leaf *param) {
+    this->params.push_back(param);
+}
