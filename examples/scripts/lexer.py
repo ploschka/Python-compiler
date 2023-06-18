2cfe810b679c191f47a9fe4468e09d42684379ec
Limuran <strelgl2@gmail.com>
2023-06-18 10:27:34 +1000

    Скрипт переделан под текущие возможности парсера

--- examples/scripts/lexer.py
+++ examples/scripts/lexer.py
@@ -1,8 +1,16 @@
 def main():
     print("Hello, World!")
-
-    def sum(a: int, b: int) -> int:
+    100 + 10 < 900 * 100 / 5 and True or False
+    def sum(a, b):
         return a + b
+
+    print(sum(500, 1000.5))
 
 if __name__ == "__main__":
     main()
+
+def f():
+    print("balls")
+    if True:
+        print("balls")
+f()
