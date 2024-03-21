39e9669bbc442530e018ddfa42d6028d681d588a
Ploshka <folga12381@gmail.com>
2024-03-21 11:51:27 +1000

    Linker added, stdlib created, trash cleaned  - Linker is just a call to gcc  - stdlib added to link by linker  - ThrowErrorManager moved

---
+++ src/pycom/run.cpp
@@ -0,0 +1,9 @@
+extern "C"
+{
+    int __pymain();
+
+    int main()
+    {
+        return __pymain();
+    }
+}
--- src/pycom/stdlib.cpp
+++ src/pycom/stdlib.cpp
@@ -2,16 +2,8 @@
 
 extern "C"
 {
-
     void putf(float a)
     {
         printf("%f\n", a);
-    }
-
-    int __pymain();
-
-    int main()
-    {
-        return __pymain();
     }
 }
